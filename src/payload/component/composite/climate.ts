c01ff5fcd49176d5218ae69eba2762ffe44738bd
Shunichiro Aki <register.develop@nana4.net>
2025-01-02 08:40:57 +0900

    feat: SwingModeの実装

--- src/payload/component/composite/climate.ts
+++ src/payload/component/composite/climate.ts
@@ -1,9 +1,11 @@
+import { language } from "@/deviceConfig";
 import { Payload } from "@/payload/type";
 import { formattedPythonDict, reverseKeyValue } from "@/util/dataTransformUtil";
 import {
   assertElStateType,
   getDeviceProperties,
   getManifactureConfig,
+  isElStateType,
 } from "@/util/deviceUtil";
 import type { ApiDevice } from "echonetlite2mqtt/server/ApiTypes";
 
@@ -15,15 +17,30 @@ const OperationModeMapping: Record<string, string> = {
   circulation: "fan_only",
 };
 
+const SwingAutoValue = {
+  ja: "自動",
+  en: "Automatic",
+};
+
+const SwingModeOrder = [
+  "uppermost",
+  "upperCenter",
+  "central",
+  "lowerCenter",
+  "lowermost",
+];
+
 export function climateBuilder(apiDevice: ApiDevice): Payload {
   const operationMode = operationModeBuilder(apiDevice);
   const tempRange = tempRangeBuilder(apiDevice);
   const fanMode = fanModeBuilder(apiDevice);
+  const swingMode = swingModeBuilder(apiDevice);
 
   return {
     ...operationMode,
     ...tempRange,
     ...fanMode,
+    ...swingMode,
     temperature_state_topic: `${apiDevice.mqttTopics}/properties/targetTemperature`,
     temperature_command_topic: `${apiDevice.mqttTopics}/properties/targetTemperature/set`,
     current_temperature_topic: `${apiDevice.mqttTopics}/properties/roomTemperature`,
@@ -31,8 +48,10 @@ export function climateBuilder(apiDevice: ApiDevice): Payload {
   };
 }
 
-function getModes(apiDevice: ApiDevice): string[] {
-  const operationMode = getDeviceProperties(apiDevice, "operationMode", true);
+function operationModeBuilder(apiDevice: ApiDevice): Payload {
+  const operationMode = getDeviceProperties(apiDevice, "operationMode");
+  if (!operationMode) return {};
+
   const { data } = operationMode.schema;
   assertElStateType(data);
   const modes = data.enum
@@ -40,20 +59,8 @@ function getModes(apiDevice: ApiDevice): string[] {
     .filter(Boolean);
   modes.push("off");
 
-  return modes;
-}
-
-function operationModeBuilder(apiDevice: ApiDevice): Payload {
   return {
-    modes: getModes(apiDevice),
-    mode_command_template: `
-{% if value == 'off' %}
-  {"operationStatus": "false"}
-{% else %}
-  {% set mapping = ${formattedPythonDict(reverseKeyValue(OperationModeMapping))} %}
-  {{ {"operationStatus": "true", "operationMode": mapping[value]} | tojson }}
-{% endif %}`.trim(),
-    mode_command_topic: `${apiDevice.mqttTopics}/properties/set`,
+    modes,
     mode_state_topic: `${apiDevice.mqttTopics}/properties`,
     mode_state_template: `
 {% if value_json.operationStatus == 'false' %}
@@ -62,6 +69,15 @@ function operationModeBuilder(apiDevice: ApiDevice): Payload {
   {% set mapping = ${formattedPythonDict(OperationModeMapping)} %}
   {{ mapping.get(value_json.operationMode, 'unknown') }}
 {% endif %}`.trim(),
+    mode_command_template: `
+{% if value == 'off' %}
+  {"operationStatus": "false"}
+{% else %}
+  {% set mapping = ${formattedPythonDict(reverseKeyValue(OperationModeMapping))} %}
+  {{ {"operationStatus": "true", "operationMode": mapping[value]} | tojson }}
+{% endif %}
+`.trim(),
+    mode_command_topic: `${apiDevice.mqttTopics}/properties/set`,
   };
 }
 
@@ -81,8 +97,8 @@ function tempRangeBuilder(apiDevice: ApiDevice): Payload {
 }
 
 function fanModeBuilder(apiDevice: ApiDevice): Payload {
-  const airFlowLevel = getDeviceProperties(apiDevice, "airFlowLevel", true);
-  if (!airFlowLevel.writable) return {};
+  const airFlowLevel = getDeviceProperties(apiDevice, "airFlowLevel");
+  if (!airFlowLevel || !airFlowLevel.writable) return {};
 
   const manufacturerConfig = getManifactureConfig(apiDevice);
   const fanmodeMapping = manufacturerConfig?.climate?.fanmodeMapping;
@@ -90,14 +106,87 @@ function fanModeBuilder(apiDevice: ApiDevice): Payload {
 
   return {
     fan_modes: Object.keys(fanmodeMapping.command),
-    fan_mode_command_topic: `${apiDevice.mqttTopics}/properties/airFlowLevel/set`,
-    fan_mode_command_template: `
-{% set mapping = ${formattedPythonDict(fanmodeMapping.command)} %}
-{{ mapping[value] }}`.trim(),
     fan_mode_state_topic: `${apiDevice.mqttTopics}/properties/airFlowLevel`,
     fan_mode_state_template: `
 {% set mapping = ${formattedPythonDict(fanmodeMapping.state)} %}
 {{ mapping.get(value, 'unknown') }}
+`.trim(),
+    fan_mode_command_topic: `${apiDevice.mqttTopics}/properties/airFlowLevel/set`,
+    fan_mode_command_template: `
+{% set mapping = ${formattedPythonDict(fanmodeMapping.command)} %}
+{{ mapping[value] }}
+`.trim(),
+  };
+}
+
+function swingModeBuilder(apiDevice: ApiDevice): Payload {
+  const airFlowDirectionVertical = getDeviceProperties(
+    apiDevice,
+    "airFlowDirectionVertical",
+  );
+  const automaticControlAirFlowDirection = getDeviceProperties(
+    apiDevice,
+    "automaticControlAirFlowDirection",
+  );
+  if (
+    !airFlowDirectionVertical ||
+    !airFlowDirectionVertical.writable ||
+    !automaticControlAirFlowDirection ||
+    !automaticControlAirFlowDirection.writable
+  ) {
+    return {};
+  }
+
+  // 風向自動設定の上下AUTOと、風向上下設定が使える場合のみ利用可能
+  const { data: verticalData } = airFlowDirectionVertical.schema;
+  const { data: autoData } = automaticControlAirFlowDirection.schema;
+  if (
+    !isElStateType(verticalData) ||
+    verticalData.enum.length === 0 ||
+    !isElStateType(autoData) ||
+    !autoData.enum.some(({ name }) => name === "autoVertical")
+  ) {
+    return {};
+  }
+
+  const stateMapping: Record<string, string> = {};
+  const commandMapping: Record<string, string> = {};
+  verticalData.enum.forEach(({ name, descriptions }) => {
+    stateMapping[name] = descriptions[language];
+    commandMapping[descriptions[language]] = name;
+  });
+
+  // enum
+  const orderedSwingModes = SwingModeOrder.filter(
+    (mode) => stateMapping[mode],
+  ).map((mode) => stateMapping[mode]);
+  const unorderedSwingModes = Object.values(stateMapping).filter(
+    (mode) => !orderedSwingModes.includes(mode),
+  );
+
+  return {
+    swing_modes: [
+      SwingAutoValue[language],
+      ...orderedSwingModes,
+      ...unorderedSwingModes,
+    ],
+    swing_mode_state_topic: `${apiDevice.mqttTopics}/properties`,
+    swing_mode_state_template: `
+{% if value_json.automaticControlAirFlowDirection == 'autoVertical' %}
+  ${SwingAutoValue[language]}
+{% else %}
+  {% set mapping = ${formattedPythonDict(stateMapping)} %}
+  {{ mapping.get(value_json.airFlowDirectionVertical, 'unknown') }}
+{% endif %}
+`.trim(),
+    swing_mode_command_topic: `${apiDevice.mqttTopics}/properties/set`,
+    swing_mode_command_template: `
+{% if value == '${SwingAutoValue[language]}' %}
+    {"automaticControlAirFlowDirection": "autoVertical"}
+{% else %}
+  {% set mapping = ${formattedPythonDict(commandMapping)} %}
+    {"airFlowDirectionVertical": "{{ mapping[value] }}"}
+{% endif %}
 `.trim(),
   };
 }
